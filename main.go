eba5a3ac9535fce7eb8d51f46f7b5eee52ca2963
piregi <iregipeter4@gmailcom>
2025-12-06 11:24:15 +0300

    starting quad funcs in quadchecker

--- main.go
+++ main.go
@@ -2,3 +2,28 @@ package main
 
 func main() {
 }
+
+func QuadA(x, y int) string {
+	res := ""
+	for row := 1; row <= y; row++ {
+		for col := 1; col <= x; col++ {
+			if row == 1 && col == 1 { // Top left corner
+				res += "o"
+			} else if row == 1 && col == x { // Top right corner
+				res += "o"
+			} else if row == y && col == 1 { // Bottom left corner
+				res += "o"
+			} else if row == y && col == x { // Bottom right corner
+				res += "o"
+			} else if row == 1 || row == y { // bottom and top border
+				res += "-"
+			} else if col == 1 || col == x { // side borders
+				res += "|"
+			} else { // empty insides
+				res += " "
+			}
+		}
+		res += "\n" // new line after row
+	}
+	return res
+}
